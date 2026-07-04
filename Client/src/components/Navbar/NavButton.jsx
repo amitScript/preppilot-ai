f61bfb3f20a66091934275d399228e9cbff28b43
amitScript <amitk400733@gmail.com>
2026-07-04 16:30:14 +0530

    login page created

--- Client/src/components/Navbar/NavButton.jsx
+++ Client/src/components/Navbar/NavButton.jsx
@@ -1,10 +1,24 @@
+
+import { Link } from "react-router-dom";
+
 function NavButton() {
-    return (
-        <>
-            <button>Login</button>
-            <button>Get Started</button>
-        </>
-    );
+  return (
+    <div className="flex items-center gap-4">
+
+      <Link to="/login">
+        <button className="hidden md:block text-slate-300 hover:text-white transition">
+          Login
+        </button>
+      </Link>
+
+      <Link to="/signup">
+        <button className="rounded-lg bg-cyan-500 px-5 py-2 font-semibold text-white hover:bg-cyan-400 transition">
+          Get Started
+        </button>
+      </Link>
+
+    </div>
+  );
 }
 
 export default NavButton;
--- Client/src/routes/AppRoutes.jsx
+++ Client/src/routes/AppRoutes.jsx
@@ -1,3 +1,4 @@
+
 import { Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
@@ -7,10 +8,13 @@ function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-
       <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
 
 export default AppRoutes;
+
+
+
+
